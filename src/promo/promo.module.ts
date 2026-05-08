36da9f21c05805ae39d408d338604ecf1a8c1271
Sardor-web-dev <dzamolovsardor5@gmail.com>
2026-05-08 23:53:27 +0500

    fixed promo service error

--- src/promo/promo.module.ts
+++ src/promo/promo.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaService } from '../prisma.service';
 import { PromoController } from './promo.controller';
 import { PromoService } from './promo.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PromoController],
   providers: [PromoService, PrismaService],
 })
